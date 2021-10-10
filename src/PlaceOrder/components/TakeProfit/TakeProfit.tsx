c6824fd2dd5471358abca471b5e7dba770b49f37
Dmitriy Schukin <schukindmitriy@gmail.com>
2021-10-10 10:49:24 +0200

    initial fork commit

---
+++ src/PlaceOrder/components/TakeProfit/TakeProfit.tsx
@@ -0,0 +1,9 @@
+/* eslint @typescript-eslint/no-use-before-define: 0 */
+import React from "react";
+import "./TakeProfit.scss";
+
+const TakeProfit = () => {
+  return <div></div>;
+};
+
+export { TakeProfit };
